350cdeebec91171f4bf2966611b6f4e75fa30a45
Kia Kamangar <kia.kamangar@gmail.com>
2025-06-16 20:18:58 +0200

    Add validation and enumeration in task model

--- test/dummy/app/models/task.rb
+++ test/dummy/app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  enum status: {
+    todo: 0,
+    pending: 1,
+    in_progress: 2,
+    completed: 3,
+    on_hold: 4,
+    cancelled: 5,
+    failed: 6,
+    reopened: 7,
+  }
 end
